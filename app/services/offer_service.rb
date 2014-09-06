9c0c1604c61289e30079522e319c9fa38c19c3da
Ali Ismayilov <ali@ismailov.info>
2014-09-06 11:14:07 -0300

    OfferService#new

--- app/services/offer_service.rb
+++ app/services/offer_service.rb
@@ -2,4 +2,10 @@ class OfferService
   include HTTParty
   base_uri 'http://api.sponsorpay.com/feed/v1/offers.json'
   format :json
+
+  def initialize(uid, pub0=nil, page=nil)
+    @uid  = uid
+    @pub0 = pub0
+    @page = page
+  end
 end
--- spec/services/offer_service_spec.rb
+++ spec/services/offer_service_spec.rb
@@ -6,4 +6,9 @@ describe OfferService do
     its(:base_uri) { is_expected.to eql 'http://api.sponsorpay.com/feed/v1/offers.json' }
     its(:format)   { is_expected.to eql :json }
   end
+
+  describe 'initialization' do
+    it { expect{ OfferService.new(uid: 'player1') }.not_to raise_error }
+    it { expect{ OfferService.new(uid: 'player1', pub0: 'campaign2', page: 1) }.not_to raise_error }
+  end
 end
