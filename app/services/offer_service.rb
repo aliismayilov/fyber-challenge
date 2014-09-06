99bd102860c3f22b81ffebb909c815f4c129f744
Ali Ismayilov <ali@ismailov.info>
2014-09-06 11:32:08 -0300

    OfferService#hashkey

--- app/services/offer_service.rb
+++ app/services/offer_service.rb
@@ -3,9 +3,30 @@ class OfferService
   base_uri 'http://api.sponsorpay.com/feed/v1/offers.json'
   format :json
 
-  def initialize(uid, pub0=nil, page=nil)
-    @uid  = uid
-    @pub0 = pub0
-    @page = page
+  def initialize(uid, pub0: nil, page: nil, ps_time: nil)
+    @uid      = uid
+    @pub0     = pub0
+    @page     = page
+    @ps_time  = ps_time
+  end
+
+  def hashvalues
+    {
+      appid: appid,
+      uid: @uid,
+      ip: ip,
+      locale: locale,
+      device_id: device_id,
+      ps_time: @ps_time,
+      pub0: @pub0,
+      page: @page,
+      timestamp: Time.now.to_i
+    }.reject { |key, value| value.nil? }
+  end
+
+  def hashkey
+    string = hashvalues.sort.map { |key, value| "#{key}=#{value}" }.join('&')
+    string += "&#{api_key}"
+    Digest::SHA1.hexdigest string
   end
 end
--- spec/services/offer_service_spec.rb
+++ spec/services/offer_service_spec.rb
@@ -8,7 +8,22 @@ describe OfferService do
   end
 
   describe 'initialization' do
-    it { expect{ OfferService.new(uid: 'player1') }.not_to raise_error }
-    it { expect{ OfferService.new(uid: 'player1', pub0: 'campaign2', page: 1) }.not_to raise_error }
+    it { expect{ OfferService.new('player1') }.not_to raise_error }
+    it { expect{ OfferService.new('player1', pub0: 'campaign2', page: 1) }.not_to raise_error }
+  end
+
+  describe 'hashkey' do
+    subject(:offer_service) { OfferService.new('player1', pub0: 'campaign2', page: 2, ps_time: 1312211903) }
+    before do
+      allow(offer_service).to receive(:appid) { 157 }
+      allow(offer_service).to receive(:ip) { '212.45.111.17' }
+      allow(offer_service).to receive(:locale) { :de }
+      allow(offer_service).to receive(:device_id) { '2b6f0cc904d137be2e1730235f5664094b831186' }
+      allow(offer_service).to receive(:timestamp) { 1312553361 }
+      allow(offer_service).to receive(:api_key) { 'e95a21621a1865bcbae3bee89c4d4f84' }
+      travel_to Time.at 1312553361
+    end
+
+    its(:hashkey) { is_expected.to eql '7a2b1604c03d46eec1ecd4a686787b75dd693c4d' }
   end
 end
